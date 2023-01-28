725b3159b84f3c5d4b070dbd1df6e7dfa485858a
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-28 07:31:21 +0300

    Решение задачи №25 Семинар №4

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход 2 числа (А и В)
+// и возводит число А в натуральную степень В
+
+Console.Write("Enter a value A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a value B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int a = A;
+for (int i = 1; i < B; i++)
+{
+    A = A * a;
+}
+Console.WriteLine(A);
